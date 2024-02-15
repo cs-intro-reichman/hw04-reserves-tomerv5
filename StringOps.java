4e998b8bb5228c65aff4f09949a5bcc1b8d6d813
tomerv5 <tomer.vaener@post.runi.ac.il>
2024-02-15 11:52:02 +0200

    Added StringOps.java

--- StringOps.java
+++ StringOps.java
@@ -1,42 +1,100 @@
 public class StringOps {
-    ////////////////////////////////////////////////////////////
-    //////                                               ///////
-    //////              Reminder:                        ///////
-    //////        allowed methods                        ///////
-    //////                                               ///////
-    //////        1.charAt(int index)                    ///////
-    //////        2.length()                             ///////
-    //////        3.substring(int start)                 ///////
-    //////        4.substring(int start,int ends)        ///////
-    //////        5.indexOf(String str)                  ///////
-    //////                                               ///////
-    //////        The rest are not allowed !             ///////
-    //////        if you want to use a different         ///////
-    //////        method, and you can implement          ///////
-    //////        it using material from the course      ///////
-    //////        you need to implement a version of     ///////
-    //////        the function by yourself.              ///////
-    //////                                               ///////
-    //////        see example for substring              ///////
-    //////        in Recitation 3 question 5             ///////
-    //////                                               ///////
-    ////////////////////////////////////////////////////////////
+    /*/
+     Reminder:
+     allowed methods
+    
+     1.charAt(int index)
+     2.length()
+     3.substring(int start)
+     4.substring(int start,int ends)
+     5.indexOf(String str)
+    
+     The rest are not allowed !
+     if you want to use a different
+     method, and you can implement
+     it using material from the course
+     you need to implement a version of
+     the function by yourself.
+    
+     see example for substring
+     in Recitation 3 question 5
+    
+    */
     public static void main(String[] args) {
+        System.out.println(capVowelsLowRest("One two tHRee world"));
+        System.out.println(camelCase("  tWo    wordS"));
         
+
     }
 
-    public static String capVowelsLowRest (String string) {
-        // Write your code here:
-        return "";
+    public static String lowCharstring(String string) {
+        String newStringlow = "";
+        for (int i = 0; i < string.length(); i++) {
+            if ((string.charAt(i) >= 'A' && string.charAt(i) <= 'Z') && string.charAt(i) != ' ')
+                newStringlow += (char) (string.charAt(i) + 32);
+            else
+                newStringlow += string.charAt(i); // making it all low letters
+        }
+        return newStringlow;
+
     }
 
-    public static String camelCase (String string) {
-        // Write your code here:
-        return "";
+    public static String noSpacesstring(String string) {
+        String newString = "";
+        for (int i = 0; i < string.length(); i++) {
+            if (string.charAt(i) != ' ')
+                newString += string.charAt(i);
+        }
+        return newString;
     }
 
-    public static int[] allIndexOf (String string, char chr) {
-        // Write your code here:
-        return new int[1];
+    public static String capVowelsLowRest(String string) {
+        String newStringlow = lowCharstring(string);
+        String capVowels = "";
+        for (int i = 0; i < newStringlow.length(); i++) {
+            if (newStringlow.charAt(i) == 'a' || newStringlow.charAt(i) == 'e' || newStringlow.charAt(i) == 'i'
+                    || newStringlow.charAt(i) == 'o' || newStringlow.charAt(i) == 'u')
+                capVowels += (char) (newStringlow.charAt(i) - 32);
+            else
+                capVowels += newStringlow.charAt(i);
+        }
+        return capVowels;
+    }
+
+    public static String camelCase(String string) {
+        String newStringlow = lowCharstring(string);
+        String newStringcap = "";
+        for (int i = 0; i < newStringlow.length(); i++) {
+            if ((newStringlow.charAt(i) == ' ' && i != newStringlow.length() - 1) && (newStringlow.charAt(i + 1) >= 'a' && newStringlow.charAt(i + 1) <= 'z')){
+                newStringcap += (char) (newStringlow.charAt(i + 1) - 32);
+                i++;
+            }
+            else newStringcap += newStringlow.charAt(i);
+        }
+        newStringcap = noSpacesstring(newStringcap);
+        if(newStringcap.charAt(0) >= 'A' && newStringcap.charAt(0) <= 'Z')
+            newStringcap = (char) (newStringcap.charAt(0)  +32) + newStringcap.substring(1);
+        
+        return newStringcap;
+    }
+    
+    public static int[] allIndexOf(String string, char chr) {
+        int count = 0;
+        for(int i = 0; i < string.length(); i++)
+        {
+            if (string.charAt(i) == chr) {
+                count++;
+            }
+        }
+        int [] index = new int[count];
+        int resIndex = 0;
+        for(int i = 0; i < string.length(); i++)
+        {
+            if (string.charAt(i) == chr) {
+                index[resIndex] = i;
+                resIndex++;
+            }
+        }
+        return index;
     }
 }
